3f6be968058c72737ab47e993e4da41e2efd6714
Halil COŞDU <halilcosdu@gmail.com>
2019-11-01 23:54:54 +0300

    first commit

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
